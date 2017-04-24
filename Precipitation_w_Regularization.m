1ed60444969441534446736f1312c0d651efa9e6
Zachary <zackbarry13@gmail.com>
2017-04-24 16:25:23 -0700

    used df and dg for source rates, added legend to plot

--- Precipitation_w_Regularization.m
+++ Precipitation_w_Regularization.m
@@ -13,14 +13,14 @@
 %    P'=H_a(C-S)*(C-1)^2.
 
 
-f=@(t)t; % species A source
+f=@(t)0.1*t; % species A source
 df=@(t) 0.1; % species A source rate
-g=@(t)0.5*t; % species B source
+g=@(t)0.05*t; % species B source
 dg=@(t) 0.05; % species B source rate
 S=1; % concentration product required for precipitation
 
 h=0.1; % time step
-t=[0:h:5]; % time grid
+t=[0:h:20]; % time grid
 
 % Non-Yosida
 A=zeros(1,length(t)); % Species A solution 
@@ -32,14 +32,14 @@ P(1)=0; % Initial value
 
 du=length(t);
 for i=2:length(t)
-    C=A(i-1)*B(i-1);
+    C=A(i-1)*B(i-1);    % Check product of concentrations
     if C<S
-        du(i-1)=0;
+        du(i-1)=0;      % No precipitation if C<S
     else du(i-1)=(C-1)^2;
     end
-    A(i)=A(i-1)+h*(df(i-1)-du(i-1));%f(t(i))-f(t(i-1))-h*du(i-1); %h*(dg(t(i-1))-du(i-1));
-    B(i)=B(i-1)+g(t(i))-g(t(i-1))-h*du(i-1);
-    P(i)=P(i-1)+h*du(i-1);
+    A(i)=A(i-1)+h*(df(t(i-1))-du(i-1)); % A(t)=A(t-1)+h*dA(t-1), 
+    B(i)=B(i-1)+h*(dg(t(i-1))-du(i-1)); %   dA(t-1)=dSource(t-1)-dPrec(t-1)
+    P(i)=P(i-1)+h*du(i-1);          % No additional source term for P
 end
 
 YA=zeros(1,length(t)); % Species A solution
@@ -50,24 +50,25 @@ YP=zeros(1,length(t)); % Precipitation solution
 YP(1)=0; % Initial value
 
 Ydu=length(t);
-a=0.5;
+a=0.1;      % Yosida parameter
 for i=2:length(t)
     C=YA(i-1)*YB(i-1);
     if C<S
         Ydu(i-1)=0;
-    else if (S<=C)&&(C<=S+a)
+    else if (S<=C)&&(C<=S+a)        % Yosida regularization
             Ydu(i-1)=(1/a)*(C-1)^2;
         else Ydu(i-1)=(C-1)^2;
         end
     end
-    YA(i)=YA(i-1)+f(t(i))-f(t(i-1))-h*Ydu(i-1);
-    YB(i)=YB(i-1)+g(t(i))-g(t(i-1))-h*Ydu(i-1);
+    YA(i)=YA(i-1)+h*(df(t(i-1))-Ydu(i-1));
+    YB(i)=YB(i-1)+h*(dg(t(i-1))-Ydu(i-1));
     YP(i)=YP(i-1)+h*Ydu(i-1);
 end
 
-
 figure
-plot(t,A,'r',t,B,'b',t,P,'k',t,YA,'r-o',t,YB,'b-o',t,YP,'k-o')
-    
+plot(t,A,'r',t,B,'b',t,P,'k',t,YA,'r--',t,YB,'b--',t,YP,'k--')
+xlabel('t');ylabel('Concentration');
+lgnd=legend('A','B','P','Yosida A','Yosida B','Yosida P');
+lgnd.Location='northwest';
     
         
