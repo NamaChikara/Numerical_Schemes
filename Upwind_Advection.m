ea74399197d5402b85b5591201590d8e9d2f6d28
Zachary <zackbarry13@gmail.com>
2017-05-02 14:05:00 -0700

    changed dramatically to accomodate Riemann Problem.  it works with animation, but when copied to Riemann_Problem.m (uploaded a minute ago), it does not work

--- Upwind_Advection.m
+++ Upwind_Advection.m
@@ -1,4 +1,4 @@
-function err=Upwind_Advection(a)
+function err=Upwind_Advection(a,tpoints)
 
 % Method is only stable if |ak/h|\leq 1
 % Recall that h=delta(x), k=delta(t)
@@ -7,35 +7,41 @@ if (a==0)
 	error('Please select a nonzero a.')
 end
 
-m=20;		% # time steps
-l=20;		% # x steps
-k=0.1;		% dt
-h=2*pi/l;	% dx
-x=[0:h:2*pi]; % l+1 length x-grid
+l=90; % number of x grid points
+xe=30; % final x value
+h=xe/l; % dx
+x=[0:h:xe]; % x grid
+
+m=tpoints; % number of t grid points
+te=2; % final t value
+k=te/m; % dt
+t=[0:k:te]; % t grid
+
+vUp=a*k/h;  % constant for upwind
 
 if (abs(a*k/h)>1)
 	error('Please select a smaller a.')
 end
 
-u1=sin(x);	% u(x,0), given
+%u1=sin(x);	% u(x,0), given
+u1=zeros(1,length(x));
+u1(1:2*round(length(x)/xe))=1; % u(x,0)=1 if 0<=x<2
+u1(2*round(length(x)/xe)+1:end)=0;  % u(x,0)=0 if 2<x
 
-if a>0
-	for i=2:m
+sol_matrix_Up=zeros(tpoints,length(x));
+sol_matrix_Up(1,:)=u1;
+
+
+for i=2:length(t)
 	u0=u1;	% update (i-1)k solution vector
-	u1(2:l+1)=u0(2:l+1)-(a*k/h)*(u0(2:l+1)-u0(1:l));  % upwind for a>0
-	u1(1)=u1(l+1); 	% periodic BC prescribed
-    end
-else
-	for i=2:m
-	u0=u1;
-	u1(1:l)=u0(1:l)-(a*k/h)*(u0(2:l+1)-u0(1:l));  % upwind for a<0
-	u1(l+1)=u1(1);	% periodic BC
-    end
+	u1(2:l+1)=u0(2:l+1)-vUp*(u0(2:l+1)-u0(1:l));  % upwind for a>0
+	u1(1)=1; 	% Reimann condition
+    sol_matrix_Up(i,:)=u1;
 end
 
-plot(x,u1,'-*')
-hold on
-plot(x,sin(x-a*k*m),'k')  % exact solution: sin(x-a*t_final)
-title('Upwind Scheme vs. Exact Solution')
+figure 
 
-err=norm(sin(x-a*k*m)-u1);
+for k = 1:tpoints
+    plot(x,sol_matrix_Up(k,:),'-') 
+    pause(0.05);     
+end
